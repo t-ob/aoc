3debdbeb4bd223a05d6c56aae1563f3115f24a91
Thomas O'Brien <thomas.obrien86@gmail.com>
2022-12-01 11:15:21 +0000

    22 day 1

--- aoc2022/src/bin/y22d1.rs
+++ aoc2022/src/bin/y22d1.rs
@@ -1,3 +1,14 @@
-fn main() {
+use std::collections::BinaryHeap;
 
+fn main() {
+    let inventories = common::io::collect_stdin_lines::<u32>("\n\n");
+
+    let mut heap =
+        BinaryHeap::<u32>::from_iter(inventories.iter().map(|inventory| inventory.iter().sum()));
+
+    let part_1 = *heap.peek().unwrap();
+    let part_2 = heap.pop().unwrap() + heap.pop().unwrap() + heap.pop().unwrap();
+
+    println!("{}", part_1);
+    println!("{}", part_2);
 }
